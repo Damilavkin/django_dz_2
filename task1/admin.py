d5f4f332a45469e4d2ea47cc48287e0407825e89
Damilavkin <Gromasht@gmail.com>
2024-09-01 23:59:41 +1000

    2 commit

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Buyer,Game
 
-# Register your models here.
+
+
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'balance', 'age']
+
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cost', 'size', 'description', 'age_limited',]
+
+
+admin.site.register(Buyer, BuyerAdmin)
+admin.site.register(Game, GameAdmin)
